3a9d8e512f4ed71e647ab7104a2328729655bb9e
Dmitriy M Riertis <moseij@yandex.ru>
2025-08-02 19:54:53 +0300

    Выполнил задание по сравнениям

---
+++ homeWork3.js
@@ -0,0 +1,45 @@
+let a = 10;
+console.log("1");
+console.log(1 > 0); //true
+
+console.log("2");
+console.log(!(1 < 0)); //true
+
+console.log("3");
+console.log('a' == a); //false
+
+console.log("4");
+console.log('a' !== a); //true
+a = 4;
+console.log("5");
+console.log(a == 10); //false
+
+console.log("6");
+console.log(a == '10'); //false
+
+console.log("7");
+console.log(a >= 4); //true
+
+console.log("8");
+console.log('4' == a);//true
+
+console.log("9");
+console.log(!(1 === '1')); // true
+
+console.log("10");
+console.log(!!true); //true
+
+console.log("11");
+console.log(!!!('0' == 0)); //false
+
+console.log("12");
+console.log(false == !false); //false
+
+console.log("13");
+console.log(1 != '1'); //false
+
+console.log("14");
+console.log(1 !== '1'); //true
+
+console.log("15");
+console.log(1 != '0'); //true
